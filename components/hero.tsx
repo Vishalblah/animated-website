548f3da72920e33baf00a606ac0d40f19d147c16
vishal singh <vishalsingh18sep@sep.com>
2026-03-26 16:51:39 +0530

    added middle video

--- components/hero.tsx
+++ components/hero.tsx
@@ -3,10 +3,14 @@ import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import { SplitText } from "gsap/all";
 import ScrollTrigger from "gsap/ScrollTrigger";
+import { useRef } from "react";
+import { useMediaQuery } from "react-responsive";
 
 gsap.registerPlugin(ScrollTrigger);
 
 export default function Hero() {
+    const videoRef = useRef<HTMLVideoElement | null>(null);
+    const isMobile = useMediaQuery({maxWidth:767})
     useGSAP(()=>{
         const heroSplit = new SplitText(".title",{type:"chars,words"});
         const paragraphSplit = new SplitText(".subtitles",{type:"lines"});
@@ -37,9 +41,30 @@ export default function Hero() {
     })
     .to('.right-leaf',{y:200},0)
     .to('.left-leaf',{y:-200},0)
-            
+    
+    const startValue = isMobile ? 'top 50%' : 'center 60%'
+    const endValue = isMobile ? '120% top' : 'bottom top'
+
+    
+
+    const tl = gsap.timeline({
+        scrollTrigger:{
+            trigger:'video',
+            start: startValue,
+            end: endValue,
+            scrub:true,
+            pin:true
+        }
+    })
+
+    videoRef.current.onloadeddata = ()=>{
+        tl.to(videoRef.current,{
+            currentTime: videoRef.current.duration
+        })
+    }
     },[]);
     return (
+        <>
         <section id="hero" className="noisy">
            <h1 className="title">MOJITO</h1>
            <img src="/images/hero-left-leaf.png" alt="hero-left-leaf" className="left-leaf"/>
@@ -65,5 +90,13 @@ export default function Hero() {
             </div>
            </div>
         </section>
+        <div className="video absolute inset-0">
+            <video ref={videoRef}
+                   src="/videos/output.mp4"
+                   muted
+                   playsInline
+                   preload="auto"/>
+        </div>
+        </>
     );
 }
